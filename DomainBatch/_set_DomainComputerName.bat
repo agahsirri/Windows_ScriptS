f710f42d5de540aa4bf5dc712e37d3ad418492c7
agahsirri <61800807+agahsirri@users.noreply.github.com>
2021-02-19 14:48:50 -0500

    Uploading files to DomainBatch v1.0.1

--- DomainBatch/_set_DomainComputerName.bat
+++ DomainBatch/_set_DomainComputerName.bat
@@ -1,1 +1,27 @@
+@ECHO off
+CLS
+COLOR 0C
+ECHO            ________________________LEGAL DISCLAIMER AND NOTICES_______________________
+ECHO           ^|                                                                          ^|
+ECHO           ^|THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR^|
+ECHO           ^| IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, ^|
+ECHO           ^| FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL  ^|
+ECHO           ^|THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER^|
+ECHO           ^| LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING  ^|
+ECHO           ^|   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER    ^|
+ECHO           ^|                     DEALINGS IN THE SOFTWARE.                            ^|
+ECHO           ^|                                                                          ^|
+ECHO           ^|               This code(s) is developed by Agah Sirri,                   ^|
+ECHO           ^|         AgahSirri@hotmail.com / https://linkedin.com/in/agahsirri        ^|
+ECHO           ^|                                 https://github.com/agahsirri             ^|
+ECHO           ^|                                                                          ^|
+ECHO           ^|                     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!                       ^|
+ECHO           ^|This batch file needs "_set_DomainComputerName.ps1" script file for running^|
+ECHO           ^|__________________________________________________________________________^|
+ECHO .
+ECHO [Press CTRL + C for an interrupt the process]
+TimeOut 10
+ECHO .
+COLOR 0A
+
 powershell -executionPolicy bypass -file "\\172.2.5.1\Software\ProgramS\_ScriptS\_set_DomainComputerName.ps1"
